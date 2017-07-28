749d4444e59682d12f71298beb747f240b1a4ee5
Jeremy Fee <jmfee@usgs.gov>
2017-07-28 09:49:40 -0600

    Initial pass at docker based dev

--- src/conf/config.inc.php
+++ src/conf/config.inc.php
@@ -2,10 +2,13 @@
 
 date_default_timezone_set('UTC');
 
+$CONFIG = array();
+
 $CONFIG_INI_FILE = dirname(__FILE__) . '/config.ini';
-if (!file_exists($CONFIG_INI_FILE)) {
-  trigger_error('Application not configured. Run pre-install script.');
-  exit(-1);
+if (file_exists($CONFIG_INI_FILE)) {
+  $CONFIG = parse_ini_file($CONFIG_INI_FILE);
+  echo 'Configuration file missing';
 }
 
-$CONFIG = parse_ini_file($CONFIG_INI_FILE);
+// environment overrides configuration file
+$CONFIG = array_merge($CONFIG, $_ENV);
